651bcf27e5dce24b1585cd912a0100c159479a3e
alicee88 <59049601+alicee88@users.noreply.github.com>
2021-10-19 08:11:55 +0100

    Add boundaries to player movement

--- LaserDefenderTut/Assets/Scripts/Player.cs
+++ LaserDefenderTut/Assets/Scripts/Player.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float shipSpeed = 10f;
+    [SerializeField] float padding = 0.5f;
+    float minX;
+    float maxX;
+    float minY;
+    float maxY;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetMoveBoundaries();
     }
 
     // Update is called once per frame
@@ -20,9 +25,24 @@ public class Player : MonoBehaviour
     private void Move()
     {
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * shipSpeed;
-        var newXPos = transform.position.x + deltaX;
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * shipSpeed;
+
+        var newXPos = transform.position.x + deltaX;
         var newYPos = transform.position.y + deltaY;
+
+        newXPos = Mathf.Clamp(newXPos, minX, maxX);
+        newYPos = Mathf.Clamp(newYPos, minY, maxY);
+
+
         transform.position = new Vector2(newXPos, newYPos);
+    }
+
+    private void SetMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        minX = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        maxX = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        minY = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        maxY = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
     }
 }
