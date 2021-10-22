2e63868f16384c0670c19e6152665caded239b4b
alicee88 <59049601+alicee88@users.noreply.github.com>
2021-10-22 09:53:49 +0100

    Make enemies follow waypoints

--- LaserDefenderTut/Assets/Scripts/EnemyPathing.cs
+++ LaserDefenderTut/Assets/Scripts/EnemyPathing.cs
@@ -4,17 +4,39 @@ using UnityEngine;
 
 public class EnemyPathing : MonoBehaviour
 {
-    [SerializeField] List<GameObject> wayPoints;
+    [SerializeField] List<Transform> wayPoints;
+    int wayPointIndex = 0;
+    float moveSpeed = 2.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = wayPoints[wayPointIndex].transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        if (wayPointIndex <= wayPoints.Count - 1)
+        {
+            var targetPosition = wayPoints[wayPointIndex].transform.position;
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+            if (transform.position == targetPosition)
+            {
+                wayPointIndex++;
+            }
+
+        }
+        else
+        {
+            wayPointIndex = 0;
+        }
     }
 }
