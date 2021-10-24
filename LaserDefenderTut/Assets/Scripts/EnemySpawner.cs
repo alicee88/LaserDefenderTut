4bbc94fbbf33c988aaff6fe231d2e13e0aae0cb6
alicee88 <59049601+alicee88@users.noreply.github.com>
2021-10-24 10:22:43 +0100

    Spawn all enemy waves

--- LaserDefenderTut/Assets/Scripts/EnemySpawner.cs
+++ LaserDefenderTut/Assets/Scripts/EnemySpawner.cs
@@ -10,8 +10,17 @@ public class EnemySpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        var currentWave = waveConfigs[startingWave];
-        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+        StartCoroutine(SpawnAllWaves());
+    }
+
+    private IEnumerator SpawnAllWaves()
+    {
+        for(int i = 0; i < waveConfigs.Count; i++)
+        {
+            var currentWave = waveConfigs[i];
+
+            yield return StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+        }
     }
 
     private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
