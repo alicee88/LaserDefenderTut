fe195a77ce6ff9f45e7329855b4e24bb3c4c9219
alicee88 <59049601+alicee88@users.noreply.github.com>
2021-10-25 07:19:12 +0100

    Add enemy firing

--- LaserDefenderTut/Assets/Scripts/Enemy.cs
+++ LaserDefenderTut/Assets/Scripts/Enemy.cs
@@ -1,21 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] int health = 100;
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 2f;
+    [SerializeField] GameObject laserPrefab;
+    float projectileSpeed = 10.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
     }
 
     // Update is called once per frame
     void Update()
     {
+        CountDownAndShoot();
+    }
+
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
         
+        if(shotCounter <= 0)
+        {
+            Fire();
+            shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject projectile = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
+        projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
