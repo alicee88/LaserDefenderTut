69cf9ed6abc18b21cfeb9d645d7de32d6ae15225
alicee88 <59049601+alicee88@users.noreply.github.com>
2021-10-29 08:01:17 +0100

    Add background music

---
+++ LaserDefenderTut/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        int musicPlayerCount = FindObjectsOfType<MusicPlayer>().Length;
+        if(musicPlayerCount > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+            Debug.Log("DESTROYING");
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
